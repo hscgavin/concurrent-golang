f6a62416e0cf143a6a322ec413db26e8896bf641
Gavin He <hscgavin@gmail.com>
2021-03-01 07:41:46 +1100

    Add buffer channel

--- chan.go
+++ chan.go
@@ -7,15 +7,17 @@ import (
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int)
+	ch := make(chan int, 1)
 	wg.Add(2)
 	go func(ch chan int, wg *sync.WaitGroup) {
+		fmt.Println(<-ch)
 		fmt.Println(<-ch)
 		wg.Done()
 	}(ch, wg)
 
 	go func(ch chan int, wg *sync.WaitGroup) {
 		ch <- 42
+		ch <- 45
 		wg.Done()
 	}(ch, wg)
 
