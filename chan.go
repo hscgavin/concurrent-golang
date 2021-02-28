0f6f18544d6203c28f65ce7c216428edcfb35b7f
Gavin He <hscgavin@gmail.com>
2021-03-01 07:46:58 +1100

    Update send and receive type channel signature

--- chan.go
+++ chan.go
@@ -9,13 +9,13 @@ func main() {
 	wg := &sync.WaitGroup{}
 	ch := make(chan int, 1)
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch <-chan int, wg *sync.WaitGroup) {
 		fmt.Println(<-ch)
 		fmt.Println(<-ch)
 		wg.Done()
 	}(ch, wg)
 
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch chan<- int, wg *sync.WaitGroup) {
 		ch <- 42
 		ch <- 45
 		wg.Done()
